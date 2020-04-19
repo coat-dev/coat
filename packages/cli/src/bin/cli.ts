5973ac4b7850a680ee526b62442b879dae065d15
Peter Juras <me@peterjuras.com>
2020-04-19 14:06:02 +0200

    feat(cli): add initial commander program
    Adds the commander library to manage the cli commands for coat.
    This commit adds no commands apart from retrieving the current
    @coat/cli version via `coat --version`

--- packages/cli/src/bin/cli.ts
+++ packages/cli/src/bin/cli.ts
@@ -1,3 +1,14 @@
-console.log("Hello coat");
+#!/usr/bin/env node
+import program from "commander";
 
-export default {};
+// package.json is imported with require and not an import
+// statement since using import would lead to a different folder
+// structure for the TypeScript declaration file outputs.
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { version } = require("../../package.json");
+program.version(version);
+
+program.parseAsync(process.argv).catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
