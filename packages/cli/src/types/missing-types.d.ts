22f412f23a45588a8dad085bb1fb9ff710ff93a4
Peter Juras <me@peterjuras.com>
2020-09-17 17:11:25 +0200

    chore(cli): :label: Use missing @types/single-trailing-newline
    Resolves #16

--- packages/cli/src/types/missing-types.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "single-trailing-newline" {
-  // TODO: See #16
-  // Add types to DefinitelyTyped repository
-  export default function singleTrailingNewline(source: string): string;
-}
