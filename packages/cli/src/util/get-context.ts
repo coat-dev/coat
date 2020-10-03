8378b214faa1ae4cb904354748a31259876c8114
Peter Juras <me@peterjuras.com>
2020-10-03 14:04:59 +0200

    refactor(cli): :recycle: Read all files for the coat context concurrently

--- packages/cli/src/util/get-context.ts
+++ packages/cli/src/util/get-context.ts
@@ -1,14 +1,14 @@
 import { promises as fs } from "fs";
 import path from "path";
-import { PackageJson } from "type-fest";
 import { CoatManifest } from "../types/coat-manifest";
-import { PACKAGE_JSON_FILENAME, COAT_MANIFEST_FILENAME } from "../constants";
+import { COAT_MANIFEST_FILENAME } from "../constants";
 import { getStrictCoatManifest } from "./get-strict-coat-manifest";
 import { CoatContext } from "../types/coat-context";
 import {
   getCoatGlobalLockfile,
   getCoatLocalLockfile,
 } from "../lockfiles/get-coat-lockfiles";
+import { getPackageJson } from "./get-package-json";
 
 /**
  * Retrieves and parses files that are relevant
@@ -24,30 +24,15 @@ export async function getContext(cwd: string): Promise<CoatContext> {
     coatManifestRaw,
     coatGlobalLockfile,
     coatLocalLockfile,
+    packageJson,
   ] = await Promise.all([
     fs.readFile(path.join(cwd, COAT_MANIFEST_FILENAME), "utf8"),
     getCoatGlobalLockfile(cwd),
     getCoatLocalLockfile(cwd),
+    getPackageJson(cwd),
   ]);
 
   const coatManifest: CoatManifest = JSON.parse(coatManifestRaw);
-
-  // Try to retrieve package.json if it is available
-  let packageJson: PackageJson | undefined;
-  try {
-    const packageJsonRaw = await fs.readFile(
-      path.join(cwd, PACKAGE_JSON_FILENAME),
-      "utf-8"
-    );
-    packageJson = JSON.parse(packageJsonRaw);
-  } catch (error) {
-    if (error.code !== "ENOENT") {
-      throw error;
-    }
-    // If the file is not available, packageJson will
-    // be undefined in the coatManifest
-  }
-
   const coatManifestStrict = getStrictCoatManifest(coatManifest);
 
   return {
