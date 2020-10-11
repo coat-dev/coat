df19905a5d23df2fa51f0a2137c3bd059dc806ed
Peter Juras <me@peterjuras.com>
2020-10-11 09:50:19 +0200

    feat(template-ts-package): :sparkles: Add compiler config option
    Adds a `compiler` configuration field that can be set to either `babel` or `typescript`. The template uses `typescript` by default if no option is specified.
    
    If `typescript` is used, the build files are generated using the typescript compiler and jest is using `ts-jest` to run tests.
    
    If `babel` is used, the build files are generated via babel and jest is using `babel-jest` to run tests.

--- packages/template-ts-package/src/index.ts
+++ packages/template-ts-package/src/index.ts
@@ -3,20 +3,32 @@ import path from "path";
 import {
   CoatManifestFile,
   CoatManifestFileType,
+  CoatManifestScript,
   CoatTemplate,
 } from "@coat/cli";
+
+enum CompilerOption {
+  TypeScript = "typescript",
+  Babel = "babel",
+}
+
+interface TemplateConfig {
+  compiler: CompilerOption;
+}
+
+const defaultConfig: TemplateConfig = {
+  compiler: CompilerOption.TypeScript,
+};
 
 const filesDir = path.join(__dirname, "..", "files");
 
 function getTemplateFile(
   filePath: string,
   type: CoatManifestFileType,
-  { once, destinationPath }: { once?: boolean; destinationPath?: string } = {
-    once: false,
-  }
+  { once, destinationPath }: { once?: boolean; destinationPath: string }
 ): CoatManifestFile {
   return {
-    file: destinationPath || filePath,
+    file: destinationPath,
     content: async () => {
       const fileRaw = await fs.readFile(path.join(filesDir, filePath), "utf-8");
       switch (type) {
@@ -31,113 +43,200 @@ function getTemplateFile(
   };
 }
 
-const createTemplate: CoatTemplate = ({ coatContext }) => {
-  return {
-    name: "@coat/template-ts-package",
-    files: [
-      getTemplateFile(".babelrc", CoatManifestFileType.Json),
-      getTemplateFile(".eslintrc", CoatManifestFileType.Json),
-      getTemplateFile("gitignore", CoatManifestFileType.Text, {
-        destinationPath: ".gitignore",
-      }),
-      getTemplateFile("jest.config.js", CoatManifestFileType.Text),
-      getTemplateFile("tsconfig.json", CoatManifestFileType.Json),
-      getTemplateFile("tsconfig.build.json", CoatManifestFileType.Json),
+const createTemplate: CoatTemplate = ({ coatContext, config: userConfig }) => {
+  const config = {
+    ...defaultConfig,
+    ...userConfig,
+  };
 
-      getTemplateFile("src/index.ts", CoatManifestFileType.Text, {
-        once: true,
-      }),
-      getTemplateFile("src/index.test.ts", CoatManifestFileType.Text, {
-        once: true,
-      }),
+  // Add all files that are shared across both compiler options
+  const files: CoatManifestFile[] = [
+    getTemplateFile("shared/.eslintrc", CoatManifestFileType.Json, {
+      destinationPath: ".eslintrc",
+    }),
+    getTemplateFile("shared/gitignore", CoatManifestFileType.Text, {
+      destinationPath: ".gitignore",
+    }),
+    getTemplateFile("shared/tsconfig.json", CoatManifestFileType.Json, {
+      destinationPath: "tsconfig.json",
+    }),
+    getTemplateFile("shared/tsconfig.build.json", CoatManifestFileType.Json, {
+      destinationPath: "tsconfig.build.json",
+    }),
 
-      // README with the coat project's name
-      {
-        file: "README.md",
-        type: CoatManifestFileType.Text,
-        content: `# ${coatContext.coatManifest.name}`,
-        once: true,
-      },
+    getTemplateFile("shared/src/index.ts", CoatManifestFileType.Text, {
+      once: true,
+      destinationPath: "src/index.ts",
+    }),
+    getTemplateFile("shared/src/index.test.ts", CoatManifestFileType.Text, {
+      once: true,
+      destinationPath: "src/index.test.ts",
+    }),
 
-      // Tell npm to only include build files in
-      // the resulting package
-      {
-        file: "package.json",
-        type: CoatManifestFileType.Json,
-        content: (source) => ({
-          ...source,
-          files: ["build/"],
-        }),
-      },
-    ],
-    dependencies: {
-      devDependencies: {
-        // Babel
-        "@babel/cli": "^7.11.6",
-        "@babel/core": "^7.11.6",
-        "@babel/preset-env": "^7.11.5",
-        "@babel/preset-typescript": "^7.10.4",
+    // README with the coat project's name
+    {
+      file: "README.md",
+      type: CoatManifestFileType.Text,
+      content: `# ${coatContext.coatManifest.name}`,
+      once: true,
+    },
 
-        // Eslint
-        eslint: "^7.8.1",
-        "@typescript-eslint/eslint-plugin": "^4.0.1",
-        "@typescript-eslint/parser": "^4.0.1",
-        "eslint-config-prettier": "^6.11.0",
-
-        // Jest
-        jest: "^26.4.2",
-        "@types/jest": "^26.0.13",
-        "babel-jest": "^26.3.0",
-        "jest-circus": "^26.4.2",
-
-        prettier: "^2.1.1",
-        typescript: "^4.0.2",
-        rimraf: "^3.0.2",
+    // Tell npm to only include build files in
+    // the resulting package
+    {
+      file: "package.json",
+      type: CoatManifestFileType.Json,
+      content: {
+        files: ["build/"],
       },
     },
-    scripts: [
-      {
-        id: "build-typedefs",
-        run: "tsc -p tsconfig.build.json --declaration --emitDeclarationOnly",
+  ];
+
+  // Shared dependencies
+  const devDependencies: Record<string, string> = {
+    // Eslint
+    eslint: "^7.8.1",
+    "@typescript-eslint/eslint-plugin": "^4.0.1",
+    "@typescript-eslint/parser": "^4.0.1",
+    "eslint-config-prettier": "^6.11.0",
+
+    // Jest
+    jest: "^26.4.2",
+    "@types/jest": "^26.0.13",
+    "jest-circus": "^26.4.2",
+
+    prettier: "^2.1.1",
+    typescript: "~4.0.2",
+    rimraf: "^3.0.2",
+  };
+
+  // Shared scripts
+  const scripts: CoatManifestScript[] = [
+    {
+      id: "prebuild-clean",
+      run: "rimraf build",
+      scriptName: "prebuild",
+    },
+    {
+      id: "lint-eslint",
+      run: "eslint --ext .ts --max-warnings 0 src",
+      scriptName: "lint",
+    },
+    {
+      id: "lint-prettier",
+      run: "prettier --check src",
+      scriptName: "lint",
+    },
+    {
+      id: "lint-types",
+      run: "tsc",
+      scriptName: "lint",
+    },
+    {
+      id: "test",
+      run: "jest",
+      scriptName: "test",
+    },
+    {
+      id: "prepare-sync-and-build",
+      run: "coat sync && coat run build",
+      scriptName: "prepare",
+    },
+  ];
+
+  switch (config.compiler) {
+    case CompilerOption.TypeScript:
+      // Files
+      files.push(
+        getTemplateFile(
+          "typescript/jest.config.js",
+          CoatManifestFileType.Text,
+          {
+            destinationPath: "jest.config.js",
+          }
+        ),
+
+        {
+          file: "tsconfig.json",
+          // Node.js 10 compatibility
+          // Update target once Node 10 is no longer supported
+          content: { compilerOptions: { target: "ES2018" } },
+          type: CoatManifestFileType.Json,
+        }
+      );
+
+      // Dependencies
+      //
+      // Jest
+      devDependencies["ts-jest"] = "^26.4.1";
+
+      // Scripts
+      scripts.push({
+        id: "build-typescript",
+        run: "tsc -p tsconfig.build.json",
         scriptName: "build",
-      },
-      {
-        id: "build-babel",
-        run:
-          'babel src -d build --extensions ".ts,.tsx" --ignore "**/*.test.ts,**/*.test.tsx"',
-        scriptName: "build",
-      },
-      {
-        id: "prebuild-clean",
-        run: "rimraf build",
-        scriptName: "prebuild",
-      },
-      {
-        id: "lint-eslint",
-        run: 'eslint "src/**/*.{ts,tsx}" --max-warnings 0',
-        scriptName: "lint",
-      },
-      {
-        id: "lint-prettier",
-        run: "prettier --check src",
-        scriptName: "lint",
-      },
-      {
-        id: "lint-types",
-        run: "tsc",
-        scriptName: "lint",
-      },
-      {
-        id: "test",
-        run: "jest",
-        scriptName: "test",
-      },
-      {
-        id: "prepare-sync-and-build",
-        run: "coat sync && coat run build",
-        scriptName: "prepare",
-      },
-    ],
+      });
+      break;
+    case CompilerOption.Babel:
+      // Files
+      files.push(
+        getTemplateFile("babel/.babelrc", CoatManifestFileType.Json, {
+          destinationPath: ".babelrc",
+        }),
+        getTemplateFile("babel/jest.config.js", CoatManifestFileType.Text, {
+          destinationPath: "jest.config.js",
+        }),
+
+        {
+          file: "tsconfig.json",
+          content: {
+            compilerOptions: { target: "ESNext", isolatedModules: true },
+          },
+          type: CoatManifestFileType.Json,
+        },
+        {
+          file: "tsconfig.build.json",
+          content: {
+            compilerOptions: {
+              isolatedModules: false,
+              emitDeclarationOnly: true,
+            },
+          },
+          type: CoatManifestFileType.Json,
+        }
+      );
+
+      // Dependencies
+      //
+      // Babel
+      devDependencies["@babel/cli"] = "^7.11.6";
+      devDependencies["@babel/core"] = "^7.11.6";
+      devDependencies["@babel/preset-env"] = "^7.11.5";
+      devDependencies["@babel/preset-typescript"] = "^7.10.4";
+      // Jest
+      devDependencies["babel-jest"] = "^26.3.0";
+
+      // Scripts
+      scripts.push(
+        {
+          id: "build-typedefs",
+          run: "tsc -p tsconfig.build.json",
+          scriptName: "build",
+        },
+        {
+          id: "build-babel",
+          run: 'babel src -d build --extensions ".ts" --ignore "**/*.test.ts"',
+          scriptName: "build",
+        }
+      );
+      break;
+  }
+
+  return {
+    name: "@coat/template-ts-package",
+    files,
+    dependencies: { devDependencies },
+    scripts,
   };
 };
 
