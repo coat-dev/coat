9d6a21f8cf8d5c5fa94024297eaa917bcf810eb2
Peter Juras <me@peterjuras.com>
2020-10-11 12:36:32 +0200

    feat(template-ts-package): :wrench: Use json instead of js for jest config (#62)
    Changes the jest.config.js file to jest.config.json to make customizations easier.

--- packages/template-ts-package/files/babel/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.ts"],
-  coverageReporters: ["text"],
-  modulePathIgnorePatterns: ["<rootDir>/build"],
-  testEnvironment: "node",
-  testRunner: "jest-circus/runner",
-};
