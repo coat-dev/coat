12ae40d6e441fbad9a30198fa19c8446626e5f05
Peter Juras <me@peterjuras.com>
2021-01-24 17:21:45 +0100

    feat: :wrench: Remove automatic prepare step
    Removes the automatic prepare step of the cli and template-ts-package. The step is also removed from coat projects that are based on template-ts-package.
    
    While the prepare step made it simpler to get started with a project without running any additional commands, it also forced users to run certain lifecycle scripts like builds or coat syncs even though they just wanted to execute other scripts (e.g. lint or test).

--- packages/template-ts-package/package.json-custom.js
+++ packages/template-ts-package/package.json-custom.js
@@ -1,13 +1,8 @@
 module.exports = (packageJson) => {
   const newPackageJson = {
     ...packageJson,
-    scripts: {
-      ...packageJson.scripts,
-    },
     files: ["build/", "files/"],
   };
-
-  newPackageJson.scripts.prepare = "coat run build && coat sync";
 
   return newPackageJson;
 };
