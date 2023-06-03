a5e928af33a8f1129aeee6f82f31c41162a7472f
Peter Juras <me@peterjuras.com>
2023-06-03 08:00:58 +0200

    feat(template-ts-package): :arrow_up: update typescript to v5.1

--- packages/template-ts-package/src/index.ts
+++ packages/template-ts-package/src/index.ts
@@ -115,7 +115,7 @@ const createTemplate: CoatTemplate = ({ coatContext, config: userConfig }) => {
     "@types/jest": "^29.0.0",
 
     prettier: "^2.2.0",
-    typescript: "~5.0.2",
+    typescript: "~5.1.3",
     rimraf: "^5.0.0",
   };
 
